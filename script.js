da740ee604a3b7aa047c6347fa84075a4f57cde4
Andrei29D <andreistefan.damaschin@gmail.com>
2026-06-06 17:35:26 +0000

    index1 + bibliografie

---
+++ script.js
@@ -0,0 +1,10 @@
+const sections = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+    sections.forEach(sec => {
+        const top = sec.getBoundingClientRect().top;
+        if(top < window.innerHeight - 100){
+            sec.classList.add("show");
+        }
+    });
+});
